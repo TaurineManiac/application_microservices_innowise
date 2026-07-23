d87af0263a04620d39e4fa8d83ef7600ffee39a3
TaurineManiac <ignatladno@gmail.com>
2026-07-23 15:44:46 +0300

    Was modified two mappers
    Was added two methods - update methods.

--- user_service/src/main/java/org/example/user_service/mapper/PaymentCardMapper.java
+++ user_service/src/main/java/org/example/user_service/mapper/PaymentCardMapper.java
@@ -1,10 +1,13 @@
 package org.example.user_service.mapper;
 
 import org.example.user_service.dto.CreatePaymentCardRequest;
+import org.example.user_service.dto.PaymentCardResponse;
+import org.example.user_service.dto.UpdateCardRequest;
 import org.example.user_service.entity.PaymentCard;
 import org.example.user_service.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -20,4 +23,15 @@ public interface PaymentCardMapper {
     @Mapping(target = "active", constant = "true")
     @Mapping(target = "user", source = "user")
     PaymentCard toEntity(CreatePaymentCardRequest request, User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "number", ignore = true)
+    @Mapping(target = "expirationDate", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "updatedDate", ignore = true)
+    void updateEntity(UpdateCardRequest request, @MappingTarget PaymentCard card);
+
+    PaymentCardResponse toResponse(PaymentCard card);
 }
--- user_service/src/main/java/org/example/user_service/mapper/UserMapper.java
+++ user_service/src/main/java/org/example/user_service/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package org.example.user_service.mapper;
 
 import org.example.user_service.dto.CreateUserRequest;
+import org.example.user_service.dto.UpdateUserRequest;
 import org.example.user_service.dto.UserResponse;
 import org.example.user_service.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
@@ -16,10 +18,18 @@ public interface UserMapper {
     UserResponse toUserResponse(User user);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "publicId", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     @Mapping(target = "paymentCards", ignore = true)
     @Mapping(target = "active", constant = "true")
     User toEntity(CreateUserRequest request);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "publicId", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "paymentCards", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    void updateEntity(UpdateUserRequest request, @MappingTarget User user);
 }
