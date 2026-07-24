d9a746b3c677c647876e35a453719c2f50b4d2ba
TaurineManiac <ignatladno@gmail.com>
2026-07-24 11:24:09 +0300

    Was fixed UserSpecification
    I was fixed comparing of name(one right case it depends on case)

--- user_service/src/main/java/org/example/user_service/specification/UserSpecification.java
+++ user_service/src/main/java/org/example/user_service/specification/UserSpecification.java
@@ -6,38 +6,47 @@ import org.springframework.data.jpa.domain.Specification;
 public class UserSpecification {
 
     public static Specification<User> hasName(String name) {
-        return (root, query, criteriaBuilder) ->{
-            if(name == null || name.isEmpty()){
+        return (root, query, criteriaBuilder) -> {
+            if (name == null || name.isEmpty()) {
                 return criteriaBuilder.conjunction();
             }
-            return criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), '%' + name + '%');
+            return criteriaBuilder.like(
+                    criteriaBuilder.lower(root.get("name")),
+                    "%" + name.toLowerCase() + "%"
+            );
         };
     }
 
     public static Specification<User> hasSurname(String surname) {
-        return ((root, query, criteriaBuilder) -> {
-            if(surname == null || surname.isEmpty()){
+        return (root, query, criteriaBuilder) -> {
+            if (surname == null || surname.isEmpty()) {
                 return criteriaBuilder.conjunction();
             }
-            return criteriaBuilder.like(criteriaBuilder.lower(root.get("surname")), "%" + surname + "%");
-        });
+            return criteriaBuilder.like(
+                    criteriaBuilder.lower(root.get("surname")),
+                    "%" + surname.toLowerCase() + "%"
+            );
+        };
     }
 
     public static Specification<User> hasEmail(String email) {
-        return ((root, query, criteriaBuilder) ->  {
-            if(email == null || email.isEmpty()){
+        return (root, query, criteriaBuilder) -> {
+            if (email == null || email.isEmpty()) {
                 return criteriaBuilder.conjunction();
             }
-            return criteriaBuilder.like(criteriaBuilder.lower(root.get("email")), "%" + email + "%");
-        });
+            return criteriaBuilder.like(
+                    criteriaBuilder.lower(root.get("email")),
+                    "%" + email.toLowerCase() + "%"
+            );
+        };
     }
 
     public static Specification<User> isActive(Boolean active) {
-        return ((root, query, criteriaBuilder) ->  {
-            if(active == null){
+        return (root, query, criteriaBuilder) -> {
+            if (active == null) {
                 return criteriaBuilder.conjunction();
             }
             return criteriaBuilder.equal(root.get("active"), active);
-        });
+        };
     }
 }
