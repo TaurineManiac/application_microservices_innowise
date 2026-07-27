0b3a24fa466eedf1fcbbc9e4fce652aab13a4f7a
TaurineManiac <ignatladno@gmail.com>
2026-07-27 16:17:35 +0300

    This exception was renamed to the GenerationExcpetion

---
+++ user_service/src/main/java/org/example/user_service/exception/GenerationException.java
@@ -0,0 +1,7 @@
+package org.example.user_service.exception;
+
+public class GenerationException extends RuntimeException {
+    public GenerationException(String message) {
+        super(message);
+    }
+}
--- user_service/src/main/java/org/example/user_service/exception/PublicIdGenerationException.java
+++
@@ -1,7 +0,0 @@
-package org.example.user_service.exception;
-
-public class PublicIdGenerationException extends RuntimeException {
-    public PublicIdGenerationException(String message) {
-        super(message);
-    }
-}
