a06a421c01f08de700439fdacf882a53ae28b9b2
TaurineManiac <ignatladno@gmail.com>
2026-07-27 16:14:14 +0300

    Was crated new type of exception better handaling service
    Was crated new type of exception better handaling service.

---
+++ user_service/src/main/java/org/example/user_service/exception/PublicIdGenerationException.java
@@ -0,0 +1,7 @@
+package org.example.user_service.exception;
+
+public class PublicIdGenerationException extends RuntimeException {
+    public PublicIdGenerationException(String message) {
+        super(message);
+    }
+}
