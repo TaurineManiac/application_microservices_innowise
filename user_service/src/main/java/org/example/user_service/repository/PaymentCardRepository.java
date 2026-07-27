fb2c1d43c8cd29917a4f3bf982bcf79ba03f37d5
TaurineManiac <ignatladno@gmail.com>
2026-07-27 14:12:04 +0300

    Was added repository annotation

--- user_service/src/main/java/org/example/user_service/repository/PaymentCardRepository.java
+++ user_service/src/main/java/org/example/user_service/repository/PaymentCardRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Repository
 public interface PaymentCardRepository extends JpaRepository<PaymentCard, Long>, JpaSpecificationExecutor<PaymentCard> {
     List<PaymentCard> findAllByUser_Id(Long id);
 
--- user_service/src/main/java/org/example/user_service/repository/UserRepository.java
+++ user_service/src/main/java/org/example/user_service/repository/UserRepository.java
@@ -8,10 +8,12 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     Optional<User> getUserByEmail(@Email String email);
