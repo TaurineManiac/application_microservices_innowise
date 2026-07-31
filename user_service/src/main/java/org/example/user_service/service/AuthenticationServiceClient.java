4fffeecfe61af184585f0472dcab9960fa9fdd43
TaurineManiac <ignatladno@gmail.com>
2026-07-31 18:42:43 +0300

    Was created Fieng class, but for user service

--- user_service/src/main/java/org/example/user_service/service/AuthenticationServiceClient.java
+++ user_service/src/main/java/org/example/user_service/service/AuthenticationServiceClient.java
@@ -1,4 +1,13 @@
 package org.example.user_service.service;
 
-public class AuthenticationServiceClient {
+import org.example.user_service.dto.ValidateResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "auth-service", url = "${auth.service.url}")
+public interface AuthenticationServiceClient {
+
+    @GetMapping("/api/auth/validate")
+    ValidateResponse validateToken(@RequestHeader("Authorization") String token);
 }
