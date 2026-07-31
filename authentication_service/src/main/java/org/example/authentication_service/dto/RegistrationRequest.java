0d4f342bd11b00fe1f3164e221b881110d6452f8
TaurineManiac <ignatladno@gmail.com>
2026-07-31 18:29:59 +0300

    Was chaned naming of param, now it equals to the same param in user_service

--- authentication_service/src/main/java/org/example/authentication_service/dto/RegistrationRequest.java
+++ authentication_service/src/main/java/org/example/authentication_service/dto/RegistrationRequest.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class RegistrationRequest {
     @NotBlank
     private String name;
@@ -29,5 +31,5 @@ public class RegistrationRequest {
 
     @Past
     @NotNull
-    private LocalDate birthDate;
+    private LocalDate dateOfBirth;
 }
