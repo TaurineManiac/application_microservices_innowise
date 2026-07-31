6c0d7d17ef7ff91e31bc95cc15d7c4bbc3bcb908
TaurineManiac <ignatladno@gmail.com>
2026-07-31 18:58:13 +0300

    This class tells how to check if this jwt valid and how to approve User

--- user_service/src/main/java/org/example/user_service/config/JwtAuthenticationFilter.java
+++ user_service/src/main/java/org/example/user_service/config/JwtAuthenticationFilter.java
@@ -1,4 +1,58 @@
 package org.example.user_service.config;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.user_service.dto.ValidateResponse;
+import org.example.user_service.service.AuthenticationServiceClient;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthenticationServiceClient authServiceClient;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            try {
+                String token = authHeader.substring(7);
+                ValidateResponse validation = authServiceClient.validateToken(authHeader);
+
+                if (validation.isValid()) {
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(
+                                    validation.getPublicId().toString(),
+                                    null,
+                                    List.of(new SimpleGrantedAuthority("ROLE_" + validation.getRole()))
+                            );
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    log.debug("Authenticated user: {} with role: {}", validation.getPublicId(), validation.getRole());
+                } else {
+                    log.warn("Invalid token provided");
+                }
+            } catch (Exception e) {
+                log.error("Error validating token: {}", e.getMessage());
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
