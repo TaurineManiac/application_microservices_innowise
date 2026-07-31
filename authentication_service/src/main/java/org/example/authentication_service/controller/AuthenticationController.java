f7abc24111f0442825333ce2af73fbeddf8d84ab
TaurineManiac <ignatladno@gmail.com>
2026-07-31 18:39:57 +0300

    Was created controller with all params

--- authentication_service/src/main/java/org/example/authentication_service/controller/AuthenticationController.java
+++ authentication_service/src/main/java/org/example/authentication_service/controller/AuthenticationController.java
@@ -1,4 +1,45 @@
 package org.example.authentication_service.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.authentication_service.dto.*;
+import org.example.authentication_service.service.AuthenticationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@Slf4j
 public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@Valid @RequestBody LoginRequest request) {
+        log.info("REST request to login: {}", request.getEmail());
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthenticationResponse> refresh(@Valid @RequestBody RefreshRequest request) {
+        log.info("REST request to refresh token");
+        return ResponseEntity.ok(authService.refresh(request.getRefreshToken()));
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<ValidateResponse> validate(@RequestHeader("Authorization") String authHeader) {
+        log.debug("REST request to validate token");
+        String token = authHeader.replace("Bearer ", "");
+        return ResponseEntity.ok(authService.validateToken(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegistrationRequest request) {
+        log.info("REST request to register user: {}", request.getEmail());
+        UserResponse response = authService.registration(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
