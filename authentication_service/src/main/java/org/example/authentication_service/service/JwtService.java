713e0f66cf88acc1431acbb911904d7360fa63d0
TaurineManiac <ignatladno@gmail.com>
2026-07-31 12:57:11 +0300

    Was createde a service for validationg jwt

--- authentication_service/src/main/java/org/example/authentication_service/service/JwtService.java
+++ authentication_service/src/main/java/org/example/authentication_service/service/JwtService.java
@@ -1,4 +1,76 @@
 package org.example.authentication_service.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.example.authentication_service.enums.Role;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.access-expiration}")
+    private Long accessExpirationMs;
+
+    @Value("${jwt.refresh-expiration}")
+    private Long refreshExpirationMs;
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateAccessToken(UUID publicId, Role role) {
+        return Jwts.builder()
+                .setSubject(publicId.toString())
+                .claim("role", role.toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessExpirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+
+    public String generateRefreshToken(UUID publicId) {
+        return Jwts.builder()
+                .setSubject(publicId.toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public UUID extractPublicIdFromClaims(String token){
+        return UUID.fromString(extractAllClaims(token).getSubject());
+    }
+
+    public String extractRole(String token){
+        return extractAllClaims(token).get("role",  String.class);
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
