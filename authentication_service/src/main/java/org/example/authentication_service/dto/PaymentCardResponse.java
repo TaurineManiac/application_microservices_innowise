aa013c97fb6d0a7031e05cdec9e043f16cbedb5b
TaurineManiac <ignatladno@gmail.com>
2026-07-31 13:01:11 +0300

    Was copied from the User service, because it was in dependencies in UserResponse class

--- authentication_service/src/main/java/org/example/authentication_service/dto/PaymentCardResponse.java
+++ authentication_service/src/main/java/org/example/authentication_service/dto/PaymentCardResponse.java
@@ -1,4 +1,21 @@
 package org.example.authentication_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentCardResponse {
+    private Long id;
+    private String number;
+    private String holder;
+    private LocalDate expirationDate;
+    private Boolean active;
 }
