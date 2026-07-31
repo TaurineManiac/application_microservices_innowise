1df5acd2c8c1f93a60d5b292df6be83c1f31099a
TaurineManiac <ignatladno@gmail.com>
2026-07-31 18:30:41 +0300

    common output exception

--- authentication_service/src/main/java/org/example/authentication_service/exception/EmailAlreadyExistsException.java
+++ authentication_service/src/main/java/org/example/authentication_service/exception/EmailAlreadyExistsException.java
@@ -1,7 +1,7 @@
 package org.example.authentication_service.exception;
 
 public class EmailAlreadyExistsException extends RuntimeException {
-  public EmailAlreadyExistsException(String message) {
-    super(message);
-  }
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
 }
