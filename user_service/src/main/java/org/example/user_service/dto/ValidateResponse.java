75e297feb5a7b1530de12aa25966c131c322684b
TaurineManiac <ignatladno@gmail.com>
2026-07-31 18:27:21 +0300

    Just copied from the auth service

--- user_service/src/main/java/org/example/user_service/dto/ValidateResponse.java
+++ user_service/src/main/java/org/example/user_service/dto/ValidateResponse.java
@@ -1,4 +1,16 @@
 package org.example.user_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ValidateResponse {
+    private UUID publicId;
+    private String role;
+    private boolean valid;
 }
