8ecef134b0903758499c79f262daeebe6cb7425d
TaurineManiac <ignatladno@gmail.com>
2026-07-31 18:34:53 +0300

    It's a Feing class, it's helps to communicationg between services

--- authentication_service/src/main/java/org/example/authentication_service/service/UserServiceClient.java
+++ authentication_service/src/main/java/org/example/authentication_service/service/UserServiceClient.java
@@ -1,4 +1,14 @@
 package org.example.authentication_service.service;
 
-public class UserServiceClient {
+import org.example.authentication_service.dto.CreateUserRequest;
+import org.example.authentication_service.dto.UserResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "user-service", url = "${user.service.url}")
+public interface UserServiceClient {
+
+    @PostMapping("/api/v1/users")
+    UserResponse createUser(@RequestBody CreateUserRequest request);
 }
