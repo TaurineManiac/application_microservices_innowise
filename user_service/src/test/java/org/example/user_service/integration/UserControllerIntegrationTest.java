2a9c739e2861ded19b1527d625577d44b32e9320
TaurineManiac <ignatladno@gmail.com>
2026-08-03 00:57:55 +0300

    Was added role into tests

--- user_service/src/test/java/org/example/user_service/integration/UserControllerIntegrationTest.java
+++ user_service/src/test/java/org/example/user_service/integration/UserControllerIntegrationTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 @Transactional
+@WithMockUser(roles = "ADMIN")
 class UserControllerIntegrationTest {
 
     @Container
