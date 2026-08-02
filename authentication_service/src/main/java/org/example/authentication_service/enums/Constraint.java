a0347f7432a981fe997dee42431041cb7412a86e
TaurineManiac <ignatladno@gmail.com>
2026-08-03 00:55:54 +0300

    Was created enum with constraints

--- authentication_service/src/main/java/org/example/authentication_service/enums/Constraint.java
+++ authentication_service/src/main/java/org/example/authentication_service/enums/Constraint.java
@@ -1,4 +1,16 @@
 package org.example.authentication_service.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Constraint {
+    MAX_RETRIES_TO_CHECK_ADMIN(10),
+    RETRY_DELAY_MS_TO_CHECK_ADMIN(5000);
+
+
+    private final int value;
+
+    Constraint(int value) {
+        this.value = value;
+    }
 }
