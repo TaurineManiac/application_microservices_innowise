4615c9c9423791ed92c4c32ca4cf1c0ebd9b0a23
TaurineManiac <ignatladno@gmail.com>
2026-08-04 14:29:52 +0300

    Was added new constaint - time to live cache

--- user_service/src/main/java/org/example/user_service/constant/AppConstraint.java
+++ user_service/src/main/java/org/example/user_service/constant/AppConstraint.java
@@ -7,7 +7,8 @@ public enum AppConstraint {
     MAX_CARDS_PER_USER(5),
     MAX_RETRIES_FOR_CARD_NUMBER(100),
     MAX_RETRIES_FOR_PUBLIC_ID(10),
-    CARD_EXPIRY_YEARS(5);
+    CARD_EXPIRY_YEARS(5),
+    TIME_TO_LIVE_MINUTES_CACHE(10);
 
     private final int value;
 
