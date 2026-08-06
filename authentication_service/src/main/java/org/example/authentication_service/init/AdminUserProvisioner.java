4e5a1bc97bfc8048896a32daa7ab1489a86d3707
TaurineManiac <ignatladno@gmail.com>
2026-08-06 22:13:38 +0300

    Was created independant Provisioner class
    This class helps to create Admin. Core idea why I separated it from AdminIntitializtor:
    1. Fro avoid proxy problems;
    2. For best debugging and validation.
    This class got retry method and  recover method, that will be working if attempts were over.

--- authentication_service/src/main/java/org/example/authentication_service/init/AdminUserProvisioner.java
+++ authentication_service/src/main/java/org/example/authentication_service/init/AdminUserProvisioner.java
@@ -1,4 +1,58 @@
 package org.example.authentication_service.init;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.authentication_service.dto.CreateUserRequest;
+import org.example.authentication_service.dto.UserResponse;
+import org.example.authentication_service.entity.Credential;
+import org.example.authentication_service.enums.Role;
+import org.example.authentication_service.repository.CredentialRepository;
+import org.example.authentication_service.service.UserServiceClient;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class AdminUserProvisioner {
+
+    private final CredentialRepository credentialRepository;
+    private final UserServiceClient userServiceClient;
+    private final PasswordEncoder passwordEncoder;
+
+    @Retryable(
+            retryFor = Exception.class,
+            maxAttemptsExpression = "#{${admin.retry.maxAttempts:10}}",
+            backoff = @Backoff(delayExpression = "#{${admin.retry.delay:5000}}")
+    )
+    public UserResponse createAdminUser(String email, String password, String name, String surname) {
+        CreateUserRequest request = CreateUserRequest.builder()
+                .name(name).surname(surname)
+                .dateOfBirth(LocalDate.of(1990, 1, 1))
+                .email(email)
+                .build();
+
+        UserResponse userResponse = userServiceClient.createUser(request);
+
+        Credential credential = Credential.builder()
+                .publicId(userResponse.getPublicId())
+                .email(email)
+                .passwordHash(passwordEncoder.encode(password))
+                .role(Role.ADMIN)
+                .build();
+
+        credentialRepository.save(credential);
+        return userResponse;
+    }
+
+    @Recover
+    public UserResponse recover(Exception exception, String email, String password, String name, String surname) {
+        log.error("Failed to provision admin after retries: ", exception);
+        return null;
+    }
 }
