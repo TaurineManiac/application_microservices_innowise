a76c0f7a588d1407e01ce481aed9a55431d170a0
TaurineManiac <ignatladno@gmail.com>
2026-08-06 22:26:30 +0300

    Was added annotation for enable retry in that application

--- authentication_service/src/main/java/org/example/authentication_service/AuthenticationServiceApplication.java
+++ authentication_service/src/main/java/org/example/authentication_service/AuthenticationServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
+@EnableRetry
 public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
