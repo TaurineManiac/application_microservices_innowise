a70d4e2e0ce7a2f8c39d4239fc3229bdef2de5f2
TaurineManiac <ignatladno@gmail.com>
2026-08-06 22:06:04 +0300

    Was created mockito bean on the admin initializer

--- authentication_service/src/test/java/org/example/authentication_service/integration/IntegrationTestBase.java
+++ authentication_service/src/test/java/org/example/authentication_service/integration/IntegrationTestBase.java
@@ -1,4 +1,11 @@
 package org.example.authentication_service.integration;
 
-public class IntegrationTestBase {
+import org.example.authentication_service.init.AdminInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class IntegrationTestBase {
+    @MockitoBean
+    protected AdminInitializer adminInitializer;
 }
