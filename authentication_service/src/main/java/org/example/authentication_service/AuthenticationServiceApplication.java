15fe8e06f4989da27ab0abe83dc7f7341203e781
TaurineManiac <ignatladno@gmail.com>
2026-08-09 15:28:47 +0300

    Was enabled kafka

--- authentication_service/src/main/java/org/example/authentication_service/AuthenticationServiceApplication.java
+++ authentication_service/src/main/java/org/example/authentication_service/AuthenticationServiceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
 @EnableRetry
+@EnableKafka
 public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
