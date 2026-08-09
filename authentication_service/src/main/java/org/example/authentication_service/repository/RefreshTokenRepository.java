86b33f46402d67d43665351d2e560b81e3675bac
TaurineManiac <ignatladno@gmail.com>
2026-08-09 15:23:23 +0300

    Was enlarget list of methods of repository. Now new method find all token by public id

--- authentication_service/src/main/java/org/example/authentication_service/repository/RefreshTokenRepository.java
+++ authentication_service/src/main/java/org/example/authentication_service/repository/RefreshTokenRepository.java
@@ -3,8 +3,11 @@ package org.example.authentication_service.repository;
 import org.example.authentication_service.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
     Optional<RefreshToken> findByToken(String token);
+    List<RefreshToken> findAllByCredentialPublicId(UUID publicId);
 }
