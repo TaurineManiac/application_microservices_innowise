2e74ec93acf668dfd8ad87ef8011a4328426c69d
TaurineManiac <ignatladno@gmail.com>
2026-08-10 20:38:40 +0300

    Was created fallback for the UserServiceClient

--- order_service/src/main/java/org/example/order_service/client/UserServiceClientFallback.java
+++ order_service/src/main/java/org/example/order_service/client/UserServiceClientFallback.java
@@ -1,4 +1,23 @@
 package org.example.order_service.client;
 
-public class UserServiceClientFallback {
+import lombok.extern.slf4j.Slf4j;
+import org.example.order_service.dto.UserInfo;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class UserServiceClientFallback implements UserServiceClient {
+
+    @Override
+    public UserInfo getUserByPublicId(UUID publicId) {
+        log.warn("User Service unavailable, returning fallback for user: {}", publicId);
+        return UserInfo.builder()
+                .publicId(publicId)
+                .name("Unknown")
+                .surname("User")
+                .email("unknown@system.com")
+                .build();
+    }
 }
