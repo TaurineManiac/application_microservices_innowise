e56efb5c12eec9d72aea390d0a56c5fe637e122d
TaurineManiac <ignatladno@gmail.com>
2026-08-10 20:38:20 +0300

    Was created feing client for the user

--- order_service/src/main/java/org/example/order_service/client/UserServiceClient.java
+++ order_service/src/main/java/org/example/order_service/client/UserServiceClient.java
@@ -1,4 +1,19 @@
 package org.example.order_service.client;
 
-public class UserServiceClient {
+import org.example.order_service.dto.UserInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@FeignClient(
+        name = "user-service",
+        url = "${user.service.url:http://localhost:8080}",
+        fallback = UserServiceClientFallback.class
+)
+public interface UserServiceClient {
+
+    @GetMapping("/api/v1/users/{publicId}")
+    UserInfo getUserByPublicId(@PathVariable("publicId") UUID publicId);
 }
