58b328ab081beb42a01fff432b7a8158bf061159
TaurineManiac <ignatladno@gmail.com>
2026-08-10 20:37:08 +0300

    Was created dto for validating User and cover TT

--- order_service/src/main/java/org/example/order_service/dto/UserInfo.java
+++ order_service/src/main/java/org/example/order_service/dto/UserInfo.java
@@ -1,4 +1,19 @@
 package org.example.order_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserInfo {
+    private UUID publicId;
+    private String name;
+    private String surname;
+    private String email;
 }
