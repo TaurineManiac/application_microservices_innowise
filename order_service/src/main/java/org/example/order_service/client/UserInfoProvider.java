bef636768a8eddbbcc4eb98b97614a371ca6ec76
TaurineManiac <ignatladno@gmail.com>
2026-08-13 21:17:41 +0300

    Was created retry and fallback logic for feing user client

--- order_service/src/main/java/org/example/order_service/client/UserInfoProvider.java
+++ order_service/src/main/java/org/example/order_service/client/UserInfoProvider.java
@@ -1,4 +1,43 @@
 package org.example.order_service.client;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.order_service.dto.UserInfo;
+import org.springframework.retry.annotation.Backoff;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class UserInfoProvider {
+
+    private final UserServiceClient userServiceClient;
+
+    @Retryable(
+            value = {Exception.class},
+            maxAttemptsExpression = "${retry.maxAttempts}",
+            backoff = @Backoff(
+                    delayExpression = "${retry.delay}",
+                    multiplierExpression = "${retry.multiplier}"
+            )
+    )
+    @CircuitBreaker(name = "userService", fallbackMethod = "fallbackGetUserInfo")
+    public UserInfo getUserInfo(UUID publicId){
+        log.debug("Calling User Service for publicId: {}", publicId);
+        return userServiceClient.getUserByPublicId(publicId);
+    }
+
+    public UserInfo fallbackGetUserInfo(UUID publicId, Throwable throwable) {
+        log.warn("Fallback triggered for publicId: {}. Reason: {}", publicId, throwable.getMessage());
+        return UserInfo.builder()
+                .publicId(publicId)
+                .name("Unknown")
+                .surname("User")
+                .email("unknown@system.com")
+                .build();
+    }
 }
