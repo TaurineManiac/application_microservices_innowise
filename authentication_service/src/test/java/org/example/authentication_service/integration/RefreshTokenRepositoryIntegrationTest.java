4835e48560ce01f68cf50412be68252462139ce3
TaurineManiac <ignatladno@gmail.com>
2026-08-21 16:14:29 +0300

    lil fix of tests :

--- authentication_service/src/test/java/org/example/authentication_service/integration/RefreshTokenRepositoryIntegrationTest.java
+++ authentication_service/src/test/java/org/example/authentication_service/integration/RefreshTokenRepositoryIntegrationTest.java
@@ -46,6 +46,7 @@ public class RefreshTokenRepositoryIntegrationTest extends IntegrationTestBase {
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
         registry.add("jwt.secret", () -> "dGVzdC1zZWNyZXQta2V5LWZvci1qd3Qtc2lnbmluZy10ZXN0cy0xMjM0NTY=");
+        registry.add("internal.service.token", () -> "test-token");
     }
 
     @Autowired
