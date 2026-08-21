4256e5471906dd3936922356fe0ec3903b895fdc
TaurineManiac <ignatladno@gmail.com>
2026-08-21 16:10:09 +0300

    Was added new rollback endpoint into a feign client

--- authentication_service/src/main/java/org/example/authentication_service/service/UserServiceClient.java
+++ authentication_service/src/main/java/org/example/authentication_service/service/UserServiceClient.java
@@ -3,12 +3,17 @@ package org.example.authentication_service.service;
 import org.example.authentication_service.dto.CreateUserRequest;
 import org.example.authentication_service.dto.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @FeignClient(name = "user-service", url = "${user.service.url}")
 public interface UserServiceClient {
 
     @PostMapping("/api/v1/users")
     UserResponse createUser(@RequestBody CreateUserRequest request);
+
+    @DeleteMapping("/api/v1/users/internal/{publicId}")
+    void rollbackUser(@PathVariable("publicId") UUID publicId,
+                      @RequestHeader("X-Internal-Token") String internalToken);
 }
