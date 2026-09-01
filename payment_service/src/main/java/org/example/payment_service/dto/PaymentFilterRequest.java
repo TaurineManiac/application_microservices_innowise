9e8e1c34a9cd3d7af43bab3f183215bae2ce053a
TaurineManiac <ignatladno@gmail.com>
2026-09-01 12:55:53 +0300

    Was created common dto for filter

--- payment_service/src/main/java/org/example/payment_service/dto/PaymentFilterRequest.java
+++ payment_service/src/main/java/org/example/payment_service/dto/PaymentFilterRequest.java
@@ -1,4 +1,23 @@
 package org.example.payment_service.dto;
 
+import lombok.*;
+import org.example.payment_service.enums.PaymentStatus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PaymentFilterRequest {
+    private UUID publicUserId;
+
+    private UUID publicOrderId;
+
+    private PaymentStatus paymentStatus;
+
+    private LocalDateTime fromDate;
+    private LocalDateTime toDate;
 }
