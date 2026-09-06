2af049ed20ddd4969da0c8c660448a5662f13bd6
Taurine <taurine@gmail.com>
2026-09-06 22:10:28 +0300

    Some debug fixes

--- payment_service/src/main/java/org/example/payment_service/entity/Payment.java
+++ payment_service/src/main/java/org/example/payment_service/entity/Payment.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.example.payment_service.enums.PaymentStatus;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,29 +18,30 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "order_public_id", nullable = false)
     private UUID orderPublicId;
 
-    @Column(nullable = false)
+    @Column(name = "user_public_id", nullable = false)
     private UUID userPublicId;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
     private PaymentStatus status;
 
-    @Builder.Default
-    @Column(nullable = false)
-    private BigDecimal amount=BigDecimal.ZERO;
+    @Column(name = "amount", nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
 
     @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-
-
 }
