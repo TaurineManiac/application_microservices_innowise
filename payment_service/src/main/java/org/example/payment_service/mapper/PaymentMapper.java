92da636dac9816d0ebc635cef62f8cf81d5d95c9
Taurine <taurine@gmail.com>
2026-09-06 22:11:35 +0300

    Was created common mapper, but with transformation from list to list

--- payment_service/src/main/java/org/example/payment_service/mapper/PaymentMapper.java
+++ payment_service/src/main/java/org/example/payment_service/mapper/PaymentMapper.java
@@ -1,4 +1,27 @@
 package org.example.payment_service.mapper;
 
-public class PaymentMapper {
+import org.example.payment_service.dto.PaymentRequestEvent;
+import org.example.payment_service.dto.PaymentResponseEvent;
+import org.example.payment_service.entity.Payment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMapper{
+
+    PaymentMapper INSTANCE = Mappers.getMapper(PaymentMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    Payment toEntity(PaymentRequestEvent event);
+
+    PaymentResponseEvent toResponse(Payment payment);
+
+    List<PaymentResponseEvent> toResponseList(List<Payment> payments);
+
 }
