98974d504d0ddcfde14a5de3c1a904544d2ef563
Taurine <taurine@gmail.com>
2026-09-06 22:12:36 +0300

    Was deleted messy methods because was implemented new interface

--- payment_service/src/main/java/org/example/payment_service/repository/PaymentRepository.java
+++ payment_service/src/main/java/org/example/payment_service/repository/PaymentRepository.java
@@ -1,4 +1,28 @@
 package org.example.payment_service.repository;
 
-public class PaymentRepository {
+import feign.Param;
+import org.example.payment_service.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment,Long>, JpaSpecificationExecutor<Payment> {
+    @Query("SELECT COALESCE(SUM(p.amount), 0) FROM Payment p WHERE p.userPublicId = :userId AND p.createdAt BETWEEN :from AND :to")
+    BigDecimal getTotalAmountForUser(
+            @Param("userId") UUID publicUserId,
+            @Param("from") LocalDateTime from,
+            @Param("to") LocalDateTime to
+    );
+
+    @Query("SELECT COALESCE(SUM(p.amount), 0) FROM Payment p WHERE p.createdAt BETWEEN :from AND :to")
+    BigDecimal getTotalAmount(
+            @Param("from") LocalDateTime from,
+            @Param("to") LocalDateTime to
+    );
 }
