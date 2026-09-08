ab8cad2c8a0e2d5599202170083c6fb6c5b5c039
Taurine <taurine@gmail.com>
2026-09-09 02:17:14 +0300

    Was enabled kafka

--- payment_service/src/main/java/org/example/payment_service/PaymentServiceApplication.java
+++ payment_service/src/main/java/org/example/payment_service/PaymentServiceApplication.java
@@ -3,9 +3,11 @@ package org.example.payment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableKafka
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
