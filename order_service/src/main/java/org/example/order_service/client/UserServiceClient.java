96bb74df42ea69c32f394f308804ad4a978b707e
Taurine <taurine@gmail.com>
2026-09-09 02:29:49 +0300

    Was changed logic of sending credentials to the user service, because with feign client I always got fallback logic, because creadentials were lost when they try to reach user service

--- order_service/src/main/java/org/example/order_service/client/UserServiceClient.java
+++ order_service/src/main/java/org/example/order_service/client/UserServiceClient.java
@@ -1,20 +1,20 @@
 package org.example.order_service.client;
 
-import org.example.order_service.config.FeignClientConfig;
 import org.example.order_service.dto.UserInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.UUID;
 
-@FeignClient(
-        name = "user-service",
-        url = "${user.service.url:http://localhost:8080}",
-        configuration = FeignClientConfig.class
-)
+@FeignClient(name = "user-service", url = "${user.service.url}")
 public interface UserServiceClient {
 
     @GetMapping("/api/v1/users/{publicId}")
-    UserInfo getUserByPublicId(@PathVariable("publicId") UUID publicId);
+    UserInfo getUserByPublicId(
+            @PathVariable("publicId") UUID publicId,
+            @RequestHeader("X-User-UUID") String userUuid,
+            @RequestHeader("X-User-Role") String userRole
+    );
 }
