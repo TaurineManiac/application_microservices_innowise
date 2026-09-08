0fefa8131a6773dc260b13b1b41920eb739cc46e
Taurine <taurine@gmail.com>
2026-09-09 02:32:38 +0300

    Was enabled kafka

--- order_service/src/main/java/org/example/order_service/OrderServiceApplication.java
+++ order_service/src/main/java/org/example/order_service/OrderServiceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
 @EnableRetry
+@EnableKafka
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
