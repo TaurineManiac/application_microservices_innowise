76643fd835e31c04c54cd5efd29e5103be2ea62d
Taurine <taurine@gmail.com>
2026-09-09 02:19:21 +0300

    fix logical problem with modifinh param

--- payment_service/src/main/java/org/example/payment_service/mapper/PaymentMapper.java
+++ payment_service/src/main/java/org/example/payment_service/mapper/PaymentMapper.java
@@ -17,7 +17,6 @@ public interface PaymentMapper{
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
     @Mapping(target = "status", ignore = true)
     Payment toEntity(PaymentRequestEvent event);
 
